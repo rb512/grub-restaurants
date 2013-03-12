52bf91f635e966e6941cc6ee4c3616dbdb511596
rahul <rahul.baxi@gmail.com>
2013-03-12 02:43:41 -0700

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 GrubshireOnsite::Application.routes.draw do
 
+  devise_for :users
+
   devise_for :user_restaurants
 
   resources :menus do
@@ -7,6 +9,7 @@ GrubshireOnsite::Application.routes.draw do
   end
 
   root :to => 'menus#index'
+  match 'authenticate' => 'dashboard#authenticate', :as => 'authenticate'
   # match 'my_menu_item/:id' => 'menus#new_menu_item', :as => 'my_menu_item'
 #   match 'create_menu_item' => 'menus#create_menu_item', :as => 'create_menu_item'
 #   
