8494476aa73d0f889a7a11d022b798396c871d0f
rahul <rahul.baxi@gmail.com>
2013-06-25 00:58:21 -0700

    logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ GrubshireOnsite::Application.configure do
   config.cache_classes = true
   config.action_mailer.default_url_options = { :host => "demo.grubshire.com" }
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
