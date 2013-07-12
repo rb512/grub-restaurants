1dcb7110765d3329156a28702b09f389fdbacbf4
Ubuntu <ubuntu@ip-10-112-39-252.ec2.internal>
2013-07-12 23:37:58 +0000

    changed email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,9 +5,9 @@ GrubshireOnsite::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local     = false
   config.action_controller.perform_caching = true
-
+  config.action_mailer.default_url_options = { :host => 'restaurants.grubshire.com' }
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130707070724) do
+ActiveRecord::Schema.define(:version => 20130710070642) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -110,6 +110,16 @@ ActiveRecord::Schema.define(:version => 20130707070724) do
     t.integer  "menu_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "tablets", :force => true do |t|
+    t.integer  "restaurant_id"
+    t.string   "ip_address"
+    t.boolean  "is_server"
+    t.integer  "owner_id"
+    t.string   "serial_no"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
