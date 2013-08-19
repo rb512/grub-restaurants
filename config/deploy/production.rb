eabd57854d2931db174b1c5533c20a6156445d09
rahul <rahul.baxi@gmail.com>
2013-08-18 23:33:01 -0700

    added email config to test env

---
+++ config/deploy/production.rb
@@ -0,0 +1,15 @@
+
+# Application environment
+set :rails_env, :production
+
+
+# master is our default git branch
+set :branch, "master"
+
+# App Domain
+set :domain, "ec2-54-227-105-117.compute-1.amazonaws.com"
+ 
+# We have all components of the app on the same server
+server domain, :app, :web, :db, :primary => true
+ssh_options[:keys] = "/Users/rahulbaxi/Downloads/grubshire.pem"
+ssh_options[:forward_agent] = true
---
+++ config/deploy/staging.rb
@@ -0,0 +1,14 @@
+
+# Application environment
+set :rails_env, :test
+
+# master is our default git branch
+set :branch, "staging"
+
+# We have all components of the app on the same server
+set :domain, "ec2-54-227-154-11.compute-1.amazonaws.com"
+
+
+server domain, :app, :web, :db, :primary => true
+ssh_options[:keys] = "/Users/rahulbaxi/Downloads/grub-staging.pem"
+ssh_options[:forward_agent] = true
