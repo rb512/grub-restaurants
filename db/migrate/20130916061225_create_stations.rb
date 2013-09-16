3f920c6ab55290839aa98e7cb778a9c1fcc50cfe
rahul <rahul.baxi@gmail.com>
2013-09-15 23:18:29 -0700

    added missing migrations

---
+++ db/migrate/20130916061225_create_stations.rb
@@ -0,0 +1,11 @@
+class CreateStations < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.string :name
+      t.string :printer_ip
+      t.integer :menu_id
+      t.integer :restaurant_id
+      t.timestamps
+    end
+  end
+end
